f7ed1930b81510a6e8a8b5ab77f676ee393d7745
divya-p04 <144213049+divya-p04@users.noreply.github.com>
2023-09-23 20:26:00 +0530

    Add files via upload

---
+++ main 3.2.py
@@ -0,0 +1,16 @@
+class Student:
+
+  def __init__(self,name,roll_number,cgpa):
+    self.name = name
+    self.roll_number = roll_number
+    self.cgpa = cgpa
+
+def sort_students(student_list):
+  sorted_students = sorted(student_list,key=lambda student: student.cgpa,reverse=True)
+  return sorted_students
+
+students=[Student("Yashaswini","Y123",7.8),Student("Srinidhi","Y124",8.9),Student("Sam","y125",9.1),Student("Madhu","Y126",9.9)]
+sorted_students = sort_students(students)
+
+for student in sorted_students:
+  print("Name: {},Roll Number: {},CGPA: {}".format(student.name,student.roll_number,student.cgpa))
